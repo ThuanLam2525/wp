0bae777bf5fdc3109c7965ec257f42488253f26c
MinhDuc1234 <tranminhduc1299@gmail.com>
2020-08-30 22:14:57 +0700

    finish p3 lab6

--- myfirsttheme/wp-content/themes/firsttheme/lib/helpers.php
+++ myfirsttheme/wp-content/themes/firsttheme/lib/helpers.php
@@ -11,13 +11,13 @@ if( !function_exists('_themename_post_meta')) {
         
         /*translators %s author*/
         printf( esc_html__('By %s', '_themename'),
-            'By <a href="' . esc_url(get_author_posts_url( get_the_author_meta('ID'))) . '">' . esc_html(get_the_author()) . '</a>'
+            'By <a class = "c-post_readmore" href="' . esc_url(get_author_posts_url( get_the_author_meta('ID'))) . '">' . esc_html(get_the_author()) . '</a>'
         );
         
     }
 }
     function _themename_readmore_link(){
-        echo '<a href="' . esc_url(get_the_permalink()) . '" title="' . the_title_attribute(['echo' => false]) . '">';
+        echo '<a class="c-post__readmore" href="' . esc_url(get_the_permalink()) . '" title="' . the_title_attribute(['echo' => false]) . '">';
         
         /*translators %s title*/
         printf( 
